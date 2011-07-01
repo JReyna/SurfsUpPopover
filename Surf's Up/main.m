1540392cc1f642d37ec47b309c92eaf139924912
Steve <steve@komorka-tech.com>
2011-07-01 18:31:25 -0500

    Initial Commit

---
+++ Surf's Up/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Surf's Up
+//
+//  Created by Baranski Steve on 7/1/11.
+//  Copyright 2011 komorka technology, llc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Surf_s_UpAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    int retVal = 0;
+    @autoreleasepool {
+        retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([Surf_s_UpAppDelegate class]));
+    }
+    return retVal;
+}
